027e060c05a11f1fbe02588141c0c8be66a74466
Fedi-Nasri <fedinasri.fsb@gmail.com>
2025-05-21 10:04:41 +0100

    configure firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,16 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDemoKeyForTesting123456789",
-  authDomain: "demo-ocean-cleaner.firebaseapp.com",
-  databaseURL: "https://demo-ocean-cleaner-default-rtdb.firebaseio.com",
-  projectId: "demo-ocean-cleaner",
-  storageBucket: "demo-ocean-cleaner.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890abcdef"
+  apiKey: "AIzaSyCdjyQBAyKzMMIZlYF8Co9Y4T1NM6_RX5w",
+  authDomain: "oceancleaner-741db.firebaseapp.com",
+  databaseURL: "https://oceancleaner-741db-default-rtdb.firebaseio.com",
+  projectId: "oceancleaner-741db",
+  storageBucket: "oceancleaner-741db.firebasestorage.app",
+  messagingSenderId: "1007338556594",
+  appId: "1:1007338556594:web:94523bb9961c2156ec4dab",
+  measurementId: "G-E7SZRP20JH"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,7 +18,7 @@ const formSchema = z.object({
   username: z.string().min(3, {
     message: "Username must be at least 3 characters.",
   }),
-  password: z.string().min(6, {
+  password: z.string().min(2, {
     message: "Password must be at least 6 characters.",
   }),
 });
